8f5b8f2ee3091bf65ca0a4524f5b1a7d82bde479
andrey sushentsov <p1oneer8527@gmail.com>
2017-11-22 00:44:43 +0500

    admin panel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,44 +1,91 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-    <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
     <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <header class="bs-docs-nav navbar navbar-static-top" id=top>
-        <div class=container>
-            <div class=navbar-header>
-                <button aria-controls=bs-navbar aria-expanded=false class="collapsed navbar-toggle" data-target=#bs-navbar data-toggle=collapse type=button> <span class=sr-only>Toggle navigation</span> <span class=icon-bar></span> <span class=icon-bar></span> <span class=icon-bar></span> </button> <a href=../ class=navbar-brand>Basic Laravel</a> </div>
-            <nav class="collapse navbar-collapse" id=bs-navbar>
-                <ul class="nav navbar-nav">
-                    <li> <a href=/about/>About</a>
-                    </li>
-                    <li> <a href=/posts/>Blog</a>
-                    </li>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href='/posts/create/'>Create Post</a></li>
-                </ul>
-            </nav>
-        </div>
-    </header>
+    <div id="app">
+        <nav class="navbar navbar-inverse">
+            <div class="container">
+                <div class="navbar-header">
+
+                    <!-- Collapsed Hamburger -->
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
+                        <span class="sr-only">Toggle Navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+
+                    <!-- Branding Image -->
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        Learn Laravel
+                    </a>
+                </div>
+
+                <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="nav navbar-nav">
+                        <li> <a href=/about/>About</a>
+                        </li>
+                        <li> <a href=/posts/>Blog</a>
+                        </li>
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="nav navbar-nav navbar-right">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li><a href="{{ route('login') }}">Login</a></li>
+                            <li><a href="{{ route('register') }}">Register</a></li>
+                        @else
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu">
+                                    <li><a href='/home'>admin panel</a></li>
+                                    <li>
+                                        <a href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            Logout
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            {{ csrf_field() }}
+                                        </form>
+                                    </li>
+                                </ul>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+
     <div class="container">
         @include('elements.messages')
         @yield('content')
     </div>
 
+    <!-- Scripts -->
     <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
     <script>
         CKEDITOR.replace( 'article-ckeditor' );
     </script>
-
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
--- resources/views/pages/post.blade.php
+++ resources/views/pages/post.blade.php
@@ -6,17 +6,5 @@
         <small>Written by {{$post->created_at}}</small>
         <p>{!!$post->content!!}</p>
         <hr>
-        <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
-        {!! Form::open([
-            'action' => [
-                    'PostsController@destroy',
-                    $post->id
-                ],
-            'method' => 'POST',
-            'class' => 'pull-right'
-        ]) !!}
-            {{ Form::hidden('_method', 'DELETE')}}
-            {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
-        {!! Form::close() !!}
     </div>
 @endsection
